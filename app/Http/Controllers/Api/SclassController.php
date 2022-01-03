246ca5e6b324b659cd0550842f53bcfd79bf1047
ansalf <anissaalifia6@gmail.com>
2022-01-03 22:17:43 +0700

    percobaan

---
+++ app/Http/Controllers/Api/SclassController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Sclass;
+
+class SclassController extends Controller
+{
+    public function Index(){
+
+        $sclass = Sclass::latest()->get();
+        return response()->json($sclass);
+
+    } // End Method 
+
+
+    public function Store(Request $request){
+
+        $validateData = $request->validate([
+            'class_name' => 'required|unique:sclasses|max:25'
+        ]);
+
+            Sclass::insert([
+                'class_name' => $request->class_name,
+            ]);
+            return response('Student Class Inserted Successfully');
+
+    } // End Method 
+
+    public function Edit($id){
+
+        $sclass = Sclass::findOrFail($id);
+        return response()->json($sclass);
+
+    } // End Method 
+
+
+    public function Update(Request $request, $id){
+       
+        Sclass::findOrFail($id)->update([
+           'class_name' => $request->class_name,
+
+        ]);
+        return response('Student Class Updated Successfully');
+
+    } // End Method 
+
+
+    public function Delete($id){
+        
+        Sclass::findOrFail($id)->delete();
+        return response('Student Class Deleted Successfully');
+
+    }// End Method 
+
+
+} 
