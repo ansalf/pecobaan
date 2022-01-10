84b92c8fc70f6afba4808c1539a9e86331a6cf32
ansalf <anissaalifia6@gmail.com>
2022-01-10 19:43:56 +0700

    full

--- database/migrations/2022_01_06_013630_create_students_table.php
+++ database/migrations/2022_01_06_013630_create_students_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSectionsTable extends Migration
+class CreateStudentsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,17 @@ class CreateSectionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('sections', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
             $table->integer('class_id');
-            $table->string('section_name');
+            $table->integer('section_id');
+            $table->string('name');
+            $table->text('address');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('password');
+            $table->string('photo');
+            $table->string('gender'); 
             $table->timestamps();
         });
     }
@@ -28,6 +35,6 @@ class CreateSectionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sections');
+        Schema::dropIfExists('students');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -4,19 +4,23 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\SclassController;
 use App\Http\Controllers\Api\SubjectController;
+use App\Http\Controllers\Api\SectionController;
+use App\Http\Controllers\Api\StudentController;
 
-// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
 //     return $request->user();
 // });
 
-// Student class route
+/// Student Class Routes 
 Route::get('/class',[SclassController::class, 'Index']);
 Route::post('/class/store',[SclassController::class, 'Store']);
 Route::get('/class/edit/{id}',[SclassController::class, 'Edit']);
 Route::post('/class/update/{id}',[SclassController::class, 'Update']);
 Route::get('/class/delete/{id}',[SclassController::class, 'Delete']);
 
-// Subject Class Routes 
+
+/// Subject Class Routes 
 Route::get('/subject',[SubjectController::class, 'Index']);
 Route::post('/subject/store',[SubjectController::class, 'Store']);
 Route::get('/subject/edit/{id}',[SubjectController::class, 'SubEdit']);
@@ -29,3 +33,10 @@ Route::post('/section/store',[SectionController::class, 'SectionStore']);
 Route::get('/section/edit/{id}',[SectionController::class, 'SectionEdit']);
 Route::post('/section/update/{id}',[SectionController::class, 'SectionUpdate']);
 Route::get('/section/delete/{id}',[SectionController::class, 'SectionDelete']);
+
+/// Student Routes 
+Route::get('/student',[StudentController::class, 'StudentIndex']);
+Route::post('/student/store',[StudentController::class, 'StudentStore']);
+Route::get('/student/edit/{id}',[StudentController::class, 'StudentEdit']);
+Route::post('/student/update/{id}',[StudentController::class, 'StudentUpdate']);
+Route::get('/student/delete/{id}',[StudentController::class, 'StudentDelete']);
